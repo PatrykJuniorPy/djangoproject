e2e823b93c499f201f0fc17d8071085f8b462bad
PatrykJuniorPy <patryk_karnowski@o2.pl>
2021-07-30 11:17:41 +0200

    added loop and ifstatments for less code

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Feature():
+    id: int
+    name: str
+    details: str
+    Our_service_is_True = bool
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Feature
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our service is very quick'
+    feature1.Our_service_is_True = True
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Easy'
+    feature2.details = 'Our service is very easy to use'
+    feature2.Our_service_is_True = True
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Reliable'
+    feature3.details = 'Our service is very reliable'
+    feature3.Our_service_is_True = True
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Cheap'
+    feature4.details = 'Our service is cheap to buy'
+    feature4.Our_service_is_True = False
+
+    features = [feature1, feature2, feature3, feature4]
+
+    return render(request, 'index.html', {'features': features})
 
 
 def counter(request):
