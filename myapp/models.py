261db1e6826523f190510d78fe2f2d31728190ee
PatrykJuniorPy <patryk_karnowski@o2.pl>
2021-08-17 10:36:04 +0200

    using django features

--- myapp/models.py
+++ myapp/models.py
@@ -3,8 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class Feature():
-    id: int
-    name: str
-    details: str
-    Our_service_is_True = bool
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
--- myapp/views.py
+++ myapp/views.py
@@ -6,32 +6,7 @@ from . models import Feature
 
 
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.details = 'Our service is very quick'
-    feature1.Our_service_is_True = True
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Easy'
-    feature2.details = 'Our service is very easy to use'
-    feature2.Our_service_is_True = True
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Reliable'
-    feature3.details = 'Our service is very reliable'
-    feature3.Our_service_is_True = True
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Cheap'
-    feature4.details = 'Our service is cheap to buy'
-    feature4.Our_service_is_True = False
-
-    features = [feature1, feature2, feature3, feature4]
-
+    features = Feature.objects.all()
     return render(request, 'index.html', {'features': features})
 
 
